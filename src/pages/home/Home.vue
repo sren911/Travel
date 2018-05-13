5cc8ff4ec5ec5cff9281cb3ac4b1505bcf55db83
sren <847937710@qq.com>
2018-05-13 17:48:45 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './compintents/Header'
+export default {
+  components: {HomeHeader},
+  name: 'Home',
+  compontents: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+</style>
